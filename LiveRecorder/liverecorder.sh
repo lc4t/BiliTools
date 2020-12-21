c5271a77c005059c607ed26b844bb4eb9b161a55
tencentsh <lc4t0.0@gmail.com>
2020-12-22 03:15:43 +0800

    rm

--- LiveRecorder/liverecorder.sh
+++ LiveRecorder/liverecorder.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 read -p '输入直播间房间号:' roomid
-mkdir $roomid
+#mkdir $roomid
 while (true)
 do
 you-get -O "$roomid"_`date +%Y%m%d_%T` -o videos https://live.bilibili.com/$roomid
